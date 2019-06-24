877a7e8ffc30c98991e47cd7f9e9aeb103da3d44
John Manoogian III <jm3@jm3.net>
2019-06-24 16:44:34 -0700

    hk: whitespace, rm unused param roccoco

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -4,11 +4,14 @@ module.exports = {
     require('postcss-css-variables'), // https://github.com/MadLittleMods/postcss-css-variables
     require('postcss-nested'), // https://github.com/postcss/postcss-nested
     require('postcss-color-gray'), // https://github.com/postcss/postcss-color-gray
+
     // require('postcss-color-function'), // fucking hell i hate this shit
-    //require('postcss-color-mod-function'), // https://github.com/MadLittleMods/postcss-css-variables
+    // require('postcss-color-mod-function'), // https://github.com/MadLittleMods/postcss-css-variables
     // require('postcss-simple-vars'), // https://github.com/postcss/postcss-simple-vars
     // require('postcss-custom-properties'),
+    //
     // interesting:
+    //
     // require('postcss-preset-env'), // https://preset-env.cssdb.org/
     // require('lost'),
     // require('precss'), // https://cssnano.co/guides/getting-started/
@@ -19,10 +22,5 @@ module.exports = {
     // require('postcss-nested'),
     // require('postcss-mixins'),
     // require('postcss-calc')
-  ],
-  'watch': 'true',
-  'local-plugins': true,
-  'autoprefixer': {
-    browsers: '> 5%'
-  }
+  ]
 }
