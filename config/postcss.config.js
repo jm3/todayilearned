ff4703e7229df05386c64087c729eef3e3b805c0
John Manoogian III <jm3@jm3.net>
2019-06-23 21:46:36 -0700

    WIP: postcss + hugo not in a good place yet
    imports, vars, color functions not appearing to play nice together yet.
    need some test cases of how this hugo postcss pipeline works

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-simple-vars'),
     require('postcss-nesting'),
-    require('postcss-preset-env') // https://preset-env.cssdb.org/
+    require('postcss-css-variables'), // https://github.com/MadLittleMods/postcss-css-variables
+    require('postcss-color-function'), // fucking hell i hate this shit
+    //require('postcss-color-mod-function'), // https://github.com/MadLittleMods/postcss-css-variables
+    // require('postcss-simple-vars'), // https://github.com/postcss/postcss-simple-vars
+    // require('postcss-custom-properties'),
     // interesting:
+    // require('postcss-preset-env'), // https://preset-env.cssdb.org/
     // require('lost'),
     // require('precss'), // https://cssnano.co/guides/getting-started/
     // require('cssnano'), // https://cssnano.co/guides/getting-started/
@@ -13,9 +17,7 @@ module.exports = {
     // require('postcss-extend'),
     // require('postcss-nested'),
     // require('postcss-mixins'),
-    // require('postcss-custom-properties'),
     // require('postcss-calc')
-    // https://github.com/davidtheclark/list-selectors
   ],
   'watch': 'true',
   'local-plugins': true,
