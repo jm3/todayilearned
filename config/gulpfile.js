0d2bbe81e89bd6bf1b552c4608cfac2db6e9fc13
John Manoogian III <jm3@jm3.net>
2019-06-30 23:08:07 -0700

    save out to 4 sizes, fixup "jpeg" filenames
    JPG rules, JPEG drools

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,25 +1,56 @@
 const { src, dest, series } = require('gulp'); // parallel useful later
 const responsive = require('gulp-responsive');
-var $ = require('gulp-load-plugins');
+const rename = require('gulp-rename');
+const rm = require('gulp-rm');
 
+// const imagemin = require('gulp-imagemin');
+// const mozjpeg = require('imagemin-mozjpeg');
+
+
+// UGH: lots of duplication below because * won't work because format needs to be specified because g-r's extname rename doesn't work (forces jpeg)
 function resize_images(cb) {
-  return src('../img-src/*.{jpg,png}')
+  return src('../img-src/**.*')
     .pipe(responsive({
-      '*.jpg': {
-        width: 200,
-        rename: { 
-          suffix: '-200w'
-        },
-        format: 'jpg'
-      },
+
+      "*": [
+         {
+            width: 500,
+            rename: {suffix: "-sm"},
+         }, {
+            width: 500 * 2,
+            rename: {suffix: "-sm@2x"},
+         }, {
+            width: 1000,
+         }, {
+            width: 1000 * 2,
+            rename: {suffix: "@2x"},
+         },
+      ],
+
     }, { // Global configuration for all images
       passThroughUnused: true,
       errorOnUnusedImage: false
     }))
     .pipe(dest('../public/img'));
   cb();
-};
+}
 
-exports.resize_images = resize_images;
-exports.default = series(resize_images);
+function rename_jpegs(cb) {
+  return src('../public/img/*.jpeg', {})
+  .pipe(rename({
+    extname: '.jpg'
+  }))
+  .pipe(dest("../public/img"));
+  cb();
+}
+
+function delete_jpegs(cb) {
+  return src('../public/img/*.jpeg', { read: false })
+  .pipe( rm())
+  cb();
+}
+
+exports.rename_jpegs = rename_jpegs;
+exports.delete_jpegs = delete_jpegs;
+exports.resize_images = series(resize_images, rename_jpegs, delete_jpegs);
 
